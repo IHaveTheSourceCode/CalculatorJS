66d5bd41bd62952614f059bba57a67d5ba83bb83
IHaveTheSourceCode <marcinbureta@gmail.com>
2022-05-13 17:25:09 +0200

    js template

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
-let num1;
-let num2;
+let num1 = "";
+let num2 = "";
 let expression;
 
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector(".display");
 
+// returns calculated result
+function calculate() {
+  let num1;
+  switch (expression) {
+    case "+":
+      return +num1 + +num2;
+    case "-":
+      return +num1 - +num2;
+    case "*":
+      return +num1 * +num2;
+    case "/":
+      return +num1 / +num2;
+  }
+}
+
 function fill(button) {
   // makes expression buttons untargetable
   // if num1 is empty
-  if (num1 == undefined && isNaN(parseInt(button.textContent))) {
+  if (num1 == "" && isNaN(parseInt(button.textContent))) {
     return;
   }
 
   // clears the display and number values
   else if (button.textContent == "AC") {
     display.textContent = "";
-    num1 = undefined;
-    num2 = undefined;
+    num1 = "";
+    num2 = 0;
+  }
+
+  // equals button
+  // if expression is undefined then
+  // display.textContent = num1
+  else if (button.textContent == "=") {
+    if (expression == undefined) {
+      display.textContent = num1;
+    } else {
+      display.textContent = calculate();
+      num2 = num1;
+      num1 = "";
+    }
   }
 
   // gets the expression
-  // swaps current number to num2 permanently
-  // saves number from display
+  // saves current number no num2 variable
+  // clears num1 value
+  // if num1 is not a number then change expression only
   else if (isNaN(parseInt(button.textContent))) {
+    expression = button.textContent;
+    if (parseInt(num1) != NaN && parseInt(num2) != NaN) {
+      display.textContent == calculate();
+      alert("hi");
+    } else if (parseInt(num1) != NaN) {
+      alert("bye");
+      num2 = num1;
+      num1 = "";
+    }
   }
 
   // fills display with numbers until it's full
-  else if (parseInt(button.textContent)) {
+  else {
     if (display.textContent.length > 7) return;
     else {
       display.append(button.textContent);
